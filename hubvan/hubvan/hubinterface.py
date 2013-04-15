979e8c466ec1c77af0d9f1d737808797c8025c68
Bill Mill <bill.mill@gmail.com>
2013-04-14 22:53:53 -0400

    adding PullRequestEvent

--- hubvan/hubvan/hubinterface.py
+++ hubvan/hubvan/hubinterface.py
@@ -21,16 +21,39 @@ class DisplayEvent(object):
             print event_json
             raise
 
+    def __repr__(self): return str(self)
+
 class ForkEvent(DisplayEvent):
     def __init__(self, event_json):
         DisplayEvent.__init__(self, event_json)
-
-    def __repr__(self): return str(self)
 
     def __str__(self):
         return '<a href="{0}">{1}</a> forked <a href="{2}">{3}</a>'.format(
                 self.actor_url,
                 self.actor,
+                self.repo_url,
+                self.repo_name)
+
+class PullRequestEvent(DisplayEvent):
+    def __init__(self, event_json):
+        DisplayEvent.__init__(self, event_json)
+
+        try:
+            self.action = event_json["payload"]["action"]
+            self.pull_number = event_json["pull_request"]["number"]
+        except KeyError:
+            print event_json
+            raise
+
+        self.pull_url = "https://github.com/{0}/pull/{1}".format(self.repo_name, self.pull_number)
+
+    def __str__(self):
+        return '<a href="{0}">{1}</a> {2} <a href="{3}">pull request {4}</a> on <a href="{5}">{6}</a>'.format(
+                self.actor_url,
+                self.actor,
+                self.action,
+                self.pull_url,
+                self.pull_number,
                 self.repo_url,
                 self.repo_name)
 
@@ -43,8 +66,6 @@ class WatchEvent(DisplayEvent):
         except KeyError:
             print event_json
             raise
-
-    def __repr__(self): return str(self)
 
     def __str__(self):
         return '<a href="{0}">{1}</a> {2} watching <a href="{3}">{4}</a>'.format(
